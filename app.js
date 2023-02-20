1a45bccc1e5e8c21660718ba457fa99363d0542f
saikolisetti <97346123+saikolisetti@users.noreply.github.com>
2023-02-20 17:33:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Welcome to IT as Code, New update for yaml..!!!');
+  res.send('Welcome to IT as Code, New update for Saimanikanta yaml code..!!!');
 });
 
 app.listen(config.port, () => {
